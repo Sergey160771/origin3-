e1d81a79bb60b526c121f317812495a6406d6c0d
Sergy <9058591133@mail.ru>
2023-01-23 10:24:57 +0500

    correction task1 homework

--- src/main/java/Homework1/Task1_1.java
+++ src/main/java/Homework1/Task1_1.java
@@ -4,20 +4,20 @@ import java.util.Arrays;
 import java.util.Random;
 
 
-public class Task1_1 {
-    public static void main(String[] args) {
-        ElementMinMax();
-    }
-
-    private static void ElementMinMax() {
-
-
-        int[] array = new int[100];
-        Random random = new Random();
-
-        for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(1000);
-        }
+//public class Task1_1 {
+//    public static void main(String[] args) {
+//        ElementMinMax();
+//    }
+//
+//    private static void ElementMinMax() {
+//
+//
+//        int[] array = new int[100];
+//        Random random = new Random();
+//
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = random.nextInt(1000);
+//        }
 
         int minElement = Integer.MAX_VALUE;
         int maxElement = Integer.MIN_VALUE;
